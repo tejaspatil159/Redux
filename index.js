747ea44e0b2e9f8f31e126f9210c808f44829121
tejaspatil159 <117001605+tejaspatil159@users.noreply.github.com>
2023-12-23 00:45:35 +0530

    Cake shop

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hi");
+const redux = require("redux");
+const createStore = redux.createStore;
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+const initialState = {
+  noOfCakes: 10,
+};
+
+const cakeOrder = () => {
+  return {
+    type: CAKE_ORDERED,
+    quatity: 1,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return { ...state, noOfCakes: state.noOfCakes - 1 };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log(`Initial state`, store.getState());
+
+const unsubscribed = store.subscribe(() =>
+  console.log(`Update state`, store.getState())
+);
+
+store.dispatch(cakeOrder());
+store.dispatch(cakeOrder());
+store.dispatch(cakeOrder());
+
+unsubscribed();
